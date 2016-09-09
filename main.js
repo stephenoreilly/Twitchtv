7c931a29a702813317f284bc00bcfd4654e3587d
Stephen O'Reilly <stiofan.oreilly@gmail.com>
2016-09-10 01:11:08 +0200

    Bones of project completed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var queryArray = ["ESL_SC2", "OgamingSC2", "brunofin", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+var queryArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"]
 $.each(queryArray, function(index, query){
 	queryTwitch(query, index + 1);
 });
@@ -6,26 +6,36 @@ $.each(queryArray, function(index, query){
 function queryTwitch(query, index){
 	var queryUrl = 'https://api.twitch.tv/kraken/streams/' + query + '?callback=?'
 	$.getJSON(queryUrl, function(data) {
+		console.log(data);
+		console.log(index);
 		displayResult(data, index, query)
 	});
 }
 function displayResult(result, index, name){
-	console.log(result);
-	$("#twitch-channel-" + index + " span.name").html(name);
 	if(result.stream){
-		$("#twitch-channel-" + index + " span.game").html(result.stream.game);
+		$("#twitch-channel-" + index + " span.game").html(result.stream.channel.status);
 		$("#twitch-channel-" + index + " span.img").html("<img src='" + result.stream.channel.logo + "'/>");
+		$("#twitch-channel-" + index + " span.name").html("<a href='https://www.twitch.tv/" + name + "' target='_blank'>" + name + "</a>");
 		console.log("result.stream");
 		// displayErrorMessage();
 	}
 	else if(result.error){
+		$("#twitch-channel-" + index + " span.name").html(name);
 		$("#twitch-channel-" + index + " span.game").html("Channel no longer available");
 		console.log("result.error");
 		// displayGameInfo():
 	}
 	else{
+		setLogo(result._links.channel, index);
+		$("#twitch-channel-" + index + " span.name").html("<a href='https://www.twitch.tv/" + name + "' target='_blank'>" + name + "</a>");
 		$("#twitch-channel-" + index + " span.game").html("Offline");
 		console.log("null");
 		// displayChannelOffline();
 	}
+}
+
+function setLogo(queryUrl, index){
+	$.getJSON(queryUrl, function(data) {
+		$("#twitch-channel-" + index + " span.img").html("<img src='" + data.logo + "'/>");
+	});
 }
