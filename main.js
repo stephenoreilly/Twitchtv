8b35aca0e5f24fcc736d61d2fe0691a02c991c53
Stephen O'Reilly <stiofan.oreilly@gmail.com>
2016-09-10 12:51:37 +0200

    Refactor JS

--- main.js
+++ main.js
@@ -1,41 +1,51 @@
 var queryArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"]
 $.each(queryArray, function(index, query){
-	queryTwitch(query, index + 1);
+  queryTwitch(query, index + 1);
 });
 
 function queryTwitch(query, index){
-	var queryUrl = 'https://api.twitch.tv/kraken/streams/' + query + '?callback=?'
-	$.getJSON(queryUrl, function(data) {
-		console.log(data);
-		console.log(index);
-		displayResult(data, index, query)
-	});
+  var queryUrl = 'https://api.twitch.tv/kraken/streams/' + query + '?callback=?'
+  $.getJSON(queryUrl, function(data) {
+    console.log(data);
+    console.log(index);
+    displayResult(data, index, query)
+  });
 }
 function displayResult(result, index, name){
-	if(result.stream){
-		$("#twitch-channel-" + index + " span.game").html(result.stream.channel.status);
-		$("#twitch-channel-" + index + " span.img").html("<img src='" + result.stream.channel.logo + "'/>");
-		$("#twitch-channel-" + index + " span.name").html("<a href='https://www.twitch.tv/" + name + "' target='_blank'>" + name + "</a>");
-		console.log("result.stream");
-		// displayErrorMessage();
-	}
-	else if(result.error){
-		$("#twitch-channel-" + index + " span.name").html(name);
-		$("#twitch-channel-" + index + " span.game").html("Channel no longer available");
-		console.log("result.error");
-		// displayGameInfo():
-	}
-	else{
-		setLogo(result._links.channel, index);
-		$("#twitch-channel-" + index + " span.name").html("<a href='https://www.twitch.tv/" + name + "' target='_blank'>" + name + "</a>");
-		$("#twitch-channel-" + index + " span.game").html("Offline");
-		console.log("null");
-		// displayChannelOffline();
-	}
+  if(result.stream){
+    setDisplayForOnlineChannel(result, index, name);
+  }
+  else if(result.error){
+    setDisplayForChannelNonExist(index, name);
+  }
+  else{
+    setDisplayForOfflineChannel(result, index, name);
+  }
 }
 
+function setDisplayForOfflineChannel(result, index, name){
+  setLogo(result._links.channel, index);
+  setNameSpan(name, index);
+  setGameText(index, "Offline");
+}
+function setDisplayForOnlineChannel(result, index, name){
+  setLogo(result._links.channel, index);
+  setGameText(index, result.stream.channel.status);
+  setNameSpan(name, index);
+}
+function setDisplayForChannelNonExist(index, name){
+  $("#twitch-channel-" + index + " span.name").html(name);
+  setGameText(index, "Channel no longer available");
+}
+
+function setGameText(index, gameText){
+  $("#twitch-channel-" + index + " span.game").html(gameText);
+}
 function setLogo(queryUrl, index){
-	$.getJSON(queryUrl, function(data) {
-		$("#twitch-channel-" + index + " span.img").html("<img src='" + data.logo + "'/>");
-	});
+  $.getJSON(queryUrl, function(data) {
+    $("#twitch-channel-" + index + " span.img").html("<img src='" + data.logo + "'/>");
+  });
+}
+function setNameSpan(name, index){
+  $("#twitch-channel-" + index + " span.name").html("<a href='https://www.twitch.tv/" + name + "' target='_blank'>" + name + "</a>");
 }
